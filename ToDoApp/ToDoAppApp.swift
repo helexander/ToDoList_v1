e23d86d58f9b99329d8bca1fc2d051f88f5d98cb
Jeremy Chee <jeremychee97@gmail.com>
2021-03-29 15:49:02 +1100

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Jeremy Chee on 29/3/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
